1081141ffa1351c3d1a04230d8f4b9e96d23be30
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-20 11:16:05 +0200

    add bookingRoute into the server.js file

--- server.js
+++ server.js
@@ -6,17 +6,26 @@ require("colors");
 const connectDB = require("./db/dbinit");
 connectDB();
 
+// routes
+const bookingRoutes = require("./routes/bookingRoutes");
+
 const port = process.env.PORT || 8080;
 
 // Middlewares
 app.use(cors());
+
+// Middleware to parse JSON requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Use the booking routes
+app.use("/booking", bookingRoutes);
 
 app.get("/", (req, res) => {
   res.send("welcome to TaskMate DB ");
 });
 
+// Start the Server
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`.bgGreen.black)
 );

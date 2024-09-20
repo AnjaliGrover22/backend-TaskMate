f78bb882216537f098072f1bc6673de968b4b444
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-20 17:45:25 +0200

    add feedback routes

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ const customerRoutes = require("./routes/customerRoute");
 const professionalRoutes = require("./routes/professionalRoute"); // import Professional routes
 const dashboardRoutes = require("./routes/dashboardRoute"); // New dashboard route
 
+// routes
 const imageRoute = require("./routes/imageRoute");
 const feedbackRoutes = require("./routes/feedbackRoute");
-connectDB();
-
-// routes
 const bookingRoutes = require("./routes/bookingRoute");
+connectDB();
 
 const port = process.env.PORT || 8080;
 
@@ -36,6 +35,8 @@ app.use("/mydashboard", dashboardRoutes);
 app.use("/api", imageRoute);
 // Use the booking routes
 app.use("/", bookingRoutes);
+//Use the feedback routes
+app.use("/", feedbackRoutes);
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`.bgGreen.black)

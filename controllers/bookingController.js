0bda3344e7f72f66a8519ae66d47aa7856562d9c
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-21 22:31:05 +0200

    UTC parse date and time but due to UTC Europeans time gives by less than 1 hr

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,12 +1,40 @@
 const Booking = require("../schemas/Booking");
+const moment = require("moment"); // // need to install this package: npm install moment
 const Customer = require("../schemas/Customer");
 
+function validateTimeFormat(timeStr) {
+  const pattern = /^(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$/;
+  return pattern.test(timeStr);
+}
 // Create a new booking
 exports.createBooking = async (req, res) => {
   try {
-    const booking = new Booking(req.body);
-    await booking.save();
-    res.status(201).json(booking);
+    const bookingData = req.body;
+
+    // Parse the date string to a valid Date object
+    const parsedDate = moment(
+      bookingData.appointment_date,
+      "DD.MM.YYYY"
+    ).toDate();
+    console.log("parsedDate ", parsedDate);
+    if (!parsedDate || isNaN(parsedDate.getTime())) {
+      return res
+        .status(400)
+        .json({ error: "Invalid date format. Please use DD.MM.YYYY" });
+    }
+
+    // Validate appoint_time
+    if (!validateTimeFormat(bookingData.appoint_time)) {
+      return res.status(400).json({
+        error: "Invalid time format. Please use HH:mm (24-hour format)",
+      });
+    }
+
+    bookingData.appointment_date = parsedDate;
+
+    const newBooking = new Booking(bookingData);
+    await newBooking.save();
+    res.status(201).json(newBooking);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }

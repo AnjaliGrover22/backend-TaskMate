f7da3440aa060b98bb34d1e117d86e9cd8e6bf48
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-22 23:39:02 +0200

    add routes for feedback

--- routes/feedbackRoute.js
+++ routes/feedbackRoute.js
@@ -2,15 +2,28 @@ const express = require("express");
 const router = express.Router();
 const feedbackController = require("../controllers/feedbackController");
 
+// Create new feedback
 router.post("/", feedbackController.createFeedback);
+
+// Get all feedback
 router.get("/", feedbackController.getAllFeedbacks);
+
+// Get feedback by ID
 router.get("/:id", feedbackController.getFeedbackById);
+
+// Update feedback
 router.put("/:id", feedbackController.updateFeedback);
+
+// Delete feedback
 router.delete("/:id", feedbackController.deleteFeedback);
+
+// Get feedback by professional ID
 router.get(
   "/professional/:profId",
   feedbackController.getFeedbackByProfessional
 );
+
+// Get feedback by customer ID
 router.get("/customer/:custId", feedbackController.getFeedbackByCustomer);
 
 module.exports = router;

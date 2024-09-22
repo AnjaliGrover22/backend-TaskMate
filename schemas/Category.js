458cfd7019db498fdf6386ab25d24f6d58d74bea
AnjaliGrover22 <anjaligrover2210@gmail.com>
2024-09-22 10:22:59 +0200

    added get category  by id and name

--- schemas/Category.js
+++ schemas/Category.js
@@ -5,6 +5,7 @@ const Category = new Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
     minLength: [5, "Category length must be atleast  20 characters "],
     maxLength: [50, "Category length must be maximum  50 characters "],
   },

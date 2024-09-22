035625fe4be888ad99dd4defc1a3f5e60a298673
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-22 18:55:01 +0200

    all routes for api methods

--- routes/feedbackRoute.js
+++ routes/feedbackRoute.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 const feedbackController = require("../controllers/feedbackController");
 
-router.post("/feedback", feedbackController.createFeedback);
-router.get("/feedback", feedbackController.getAllFeedbacks);
-router.get("/feedback/:id", feedbackController.getFeedbackById);
-router.put("/feedback/:id", feedbackController.updateFeedback);
-router.delete("/feedback/:id", feedbackController.deleteFeedback);
+router.post("/", feedbackController.createFeedback);
+router.get("/", feedbackController.getAllFeedbacks);
+router.get("/:id", feedbackController.getFeedbackById);
+router.put("/:id", feedbackController.updateFeedback);
+router.delete("/:id", feedbackController.deleteFeedback);
+router.get(
+  "/professional/:profId",
+  feedbackController.getFeedbackByProfessional
+);
+router.get("/customer/:custId", feedbackController.getFeedbackByCustomer);
 
 module.exports = router;

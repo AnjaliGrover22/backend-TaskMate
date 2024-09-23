317935a874d5c89a5a3bbcc75aa070575343a8b1
AnjaliGrover22 <anjaligrover2210@gmail.com>
2024-09-23 14:44:29 +0200

    final commit

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const connectDB = require("./db/dbinit");
 //Importing all the routes below
 const customerRoutes = require("./routes/customerRoute");
 const professionalRoutes = require("./routes/professionalRoute"); // import Professional route
-const dashboardRoutes = require("./routes/dashboardRoute"); // Import dashboard route
 const serviceRoutes = require("./routes/serviceRoute"); // Import the service route
 const categoryRoute = require("./routes/categoryRoute"); //Import category route
 const feedbackRoutes = require("./routes/feedbackRoute"); //Import feedback route
@@ -30,18 +29,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Use Customer and Professional routes
-app.use("/customer", customerRoutes);
-app.use("/professional", professionalRoutes); // add Professional routes
-//app.use("/mydashboard", dashboardRoutes);
-app.use("/categories", categoryRoute);
-app.use("/api/services", serviceRoutes);
-app.use("/faqs", fAQRoute);
-app.use("/booking", bookingRoutes); // Use the booking routes
-app.use("/feedback", feedbackRoutes); //Use the feedback routes
-app.use("/favourite", favouriteRoutes); //Use the favourite routes
-app.use("/dashboard", pintoDashboardRoutes); //Use the dashboard routes
-
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
@@ -50,11 +37,11 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.send("welcome to TaskMate DB ");
 });
+
 // All Customer and Professional routes defined here
 
 app.use("/customer", customerRoutes); //Customer routes
 app.use("/professional", professionalRoutes); // Professional routes
-app.use("/mydashboard", dashboardRoutes); // dashboard routes
 app.use("/categories", categoryRoute); //Categories routes
 app.use("/services", serviceRoutes); //  services routes
 app.use("/faqs", fAQRoute); //faq routes
@@ -62,6 +49,7 @@ app.use("/booking", bookingRoutes); // Use the booking routes
 app.use("/feedback", feedbackRoutes); //Use the feedback routes
 app.use("/favourite", favouriteRoutes); // //Use the favourite routes
 app.use("/newJob", addJobRoutes); //Use the addNewJob routes
+app.use("/dashboard", pintoDashboardRoutes); //Use the dashboard routes
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`.bgGreen.black)
